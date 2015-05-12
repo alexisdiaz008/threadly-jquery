aadfc5e521f4d67ce2d0ccdfbd2d120d59c06cc7
lexmasta <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-05-12 12:33:27 -0400

    changes to script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var main = function() {
   $('form').submit(function() {
     
+    var comment === $('input:comment').val();
+    //save input val in comment variable
     if(comment !== "") {
-      var html = $('<li>').text(comment);
-      
+      var html = $('<li>').text(comment).prependTo('.comments');
+      //if its not empty make it a li and put it in .comments class
     }
 
     return false;
